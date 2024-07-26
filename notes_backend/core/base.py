b9e85d9b6bcb5a3e4a05529441e53bf5498887fa
blueoc-phucpham <phuc.pham@blueoc.tech>
2024-07-26 11:45:35 +0700

    feat: config soft-delete

--- notes_backend/core/base.py
+++ notes_backend/core/base.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.utils import timezone
+
+
+class BaseModelManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return super().get_queryset().filter(deleted_at=None)
 
 
 class Base(models.Model):
@@ -6,5 +12,11 @@ class Base(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(default=None, null=True)
 
+    objects = BaseModelManager()
+
     class Meta:
         abstract = True
+
+    def delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
